c50e85ccfa036896ea6d771b588fe256f3e11e97
Alexey Dobrynin <ag.dobrynin@gmail.com>
2019-08-28 13:45:41 +0400

    feat(helpers): add namespace for helpers function

--- src/kaspi/helpers/helpers.php
+++ src/kaspi/helpers/helpers.php
@@ -1,18 +1,25 @@
 <?php
+namespace Kaspi\helpers;
 
 use Kaspi\FlashMessages;
 
-function flashErrors(): ?array
-{
-    return FlashMessages::display(FlashMessages::ERROR);
+if (false === function_exists('Kaspi\helpers\flashErrors')) {
+    function flashErrors(): ?array
+    {
+        return FlashMessages::display(FlashMessages::ERROR);
+    }
 }
 
-function flashSuccess(): ?array
-{
-    return FlashMessages::display(FlashMessages::SUCCESS);
+if (false === function_exists('Kaspi\helpers\flashSuccess')) {
+    function flashSuccess(): ?array
+    {
+        return FlashMessages::display(FlashMessages::SUCCESS);
+    }
 }
 
-function flashWarning(): ?array
-{
-    return FlashMessages::display(FlashMessages::WARNING);
+if (false === function_exists('Kaspi\helpers\flashWarning')) {
+    function flashWarning(): ?array
+    {
+        return FlashMessages::display(FlashMessages::WARNING);
+    }
 }
