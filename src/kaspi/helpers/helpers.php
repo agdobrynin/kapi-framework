1d4f51dd40d2456334010767d0cead630a0e8116
Alexey Dobrynin <ag.dobrynin@gmail.com>
2019-08-29 10:43:37 +0400

    feat(helpers): helpers function without namespace

--- src/kaspi/helpers/helpers.php
+++ src/kaspi/helpers/helpers.php
@@ -1,37 +1,35 @@
 <?php
-namespace Kaspi\helpers;
-
 use Kaspi\FlashMessages;
 use Kaspi\Router;
 
-if (false === function_exists('Kaspi\helpers\flashErrors')) {
-    function flash(): ?array
+if (false === function_exists('flashErrors')) {
+    function flashErrors(): ?array
     {
         return FlashMessages::display(FlashMessages::ERROR);
     }
 }
 
-if (false === function_exists('Kaspi\helpers\flashSuccess')) {
+if (false === function_exists('flashSuccess')) {
     function flashSuccess(): ?array
     {
         return FlashMessages::display(FlashMessages::SUCCESS);
     }
 }
 
-if (false === function_exists('Kaspi\helpers\flashWarning')) {
+if (false === function_exists('flashWarning')) {
     function flashWarning(): ?array
     {
         return FlashMessages::display(FlashMessages::WARNING);
     }
 }
 
-if (false === function_exists('Kaspi\helpers\getCurrentRoute')) {
+if (false === function_exists('getCurrentRoute')) {
     function getCurrentRoute(): ?string {
         return Router::getCurrentRouteName();
     }
 }
 
-if (false === function_exists('Kaspi\helpers\isRoute')) {
+if (false === function_exists('isRoute')) {
     function isRoute(string $route): bool {
         return $route === getCurrentRoute();
     }
