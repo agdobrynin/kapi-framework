9dbbe96f70f84666fc4f9077b3c04cd2e427401a
Alexey Dobrynin <ag.dobrynin@gmail.com>
2019-08-31 09:19:28 +0400

    ref(Controller): request, response, container - are protected
    All property now available into children class by
    $this->request - incoming http request
    $this->response - output http response
    $this->container - application containers

--- src/kaspi/Controller.php
+++ src/kaspi/Controller.php
@@ -5,29 +5,16 @@ namespace Kaspi;
 class Controller
 {
     /** @var Container|null */
-    private $container;
-    private $request;
-    private $response;
+    protected $container;
+    /** @var Request  */
+    protected $request;
+    /** @var Response  */
+    protected $response;
 
     public function __construct(Request $request, Response $response, ?Container $container = null)
     {
         $this->request = $request;
         $this->response = $response;
         $this->container = $container;
-    }
-
-    public function getRequest(): Request
-    {
-        return $this->request;
-    }
-
-    public function getResponse(): Response
-    {
-        return $this->response;
-    }
-
-    public function getContainer(): ?Container
-    {
-        return $this->container;
     }
 }
