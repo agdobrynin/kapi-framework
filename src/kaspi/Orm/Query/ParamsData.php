09519c9a6f52253e87c0ff4ff0bf93d3e4c7b60f
Alexey Dobrynin <ag.dobrynin@gmail.com>
2019-09-04 17:31:47 +0400

    bug(ParamsData): prepared arrays value

--- src/kaspi/Orm/Query/ParamsData.php
+++ src/kaspi/Orm/Query/ParamsData.php
@@ -89,6 +89,11 @@ class ParamsData
         );
         $this->insert = implode(', ', $templateInsert);
         $this->stmData = array_combine($template, $this->sourceData);
+        foreach ($this->stmData as $key => $value) {
+            if (null === $value) {
+                $this->stmData[$key] = '';
+            }
+        }
         $this->values = implode(', ', $template);
     }
 }
